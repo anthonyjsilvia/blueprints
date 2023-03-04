1e6601a2a65785ec1cf99920149c32f8bd72ad54
Anthony Silvia <61393689+anthonyjsilvia@users.noreply.github.com>
2023-03-04 16:32:03 -0500

    Add files via upload

---
+++ gallery/js/mobile.js
@@ -0,0 +1,23 @@
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/android/i.test(userAgent)) {
+    window.location.href = "https://nodeda.com/blueprint/download";
+  }
+
+  if (/iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    window.location.href = "https://nodeda.com/blueprint/download";
+  }
+/*
+  var downloadLink = document.querySelector('.header-nav ul li a');
+
+  if (/Macintosh/.test(userAgent)) {
+    if (/Intel/.test(userAgent) || /Apple/.test(userAgent)) {
+      downloadLink.innerHTML = 'Download for Mac';
+      downloadLink.href = 'https://nodeda.com/blueprint/';
+    }
+  }
+  else if (/iPad/.test(userAgent)) {
+    downloadLink.innerHTML = 'Download for iPad';
+    downloadLink.href = 'https://apps.apple.com/us/app/recipedo-gallery/id1564537067';
+  }
+*/
